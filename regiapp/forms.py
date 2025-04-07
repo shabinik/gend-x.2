1c48a8775ceccc8d7ed2e60733c01930d7d19050
shabin <muhammedshabin2001@gmail.com>
2025-04-07 15:42:09 +0530

    second commit

--- regiapp/forms.py
+++ regiapp/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
+from . models import Referral
 import re
 
 #Registration form
 class RegistrationForm(forms.ModelForm):
     password=forms.CharField(widget=forms.PasswordInput)
     password2=forms.CharField(widget=forms.PasswordInput,label="Confirm password")
+    referral_code = forms.CharField(required=False,max_length=50)
 
 
     class Meta:
@@ -15,11 +17,21 @@ class RegistrationForm(forms.ModelForm):
     def clean_username(self):
         username=self.cleaned_data.get('username', '').strip()
 
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('This Username is already exist Use different username')
+
         if len(username) < 4:
             raise forms.ValidationError("Username should have min 4 letters")
         if " " in username:
             raise forms.ValidationError("Username cannot contains space")
         return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email','').strip()
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This Email already used please use another one')
+        return email
 
     def clean_password(self):
         password = self.cleaned_data.get('password', '')
@@ -36,7 +48,12 @@ class RegistrationForm(forms.ModelForm):
         if self.cleaned_data.get('password') != self.cleaned_data.get('password2'):
             raise forms.ValidationError("Password donot match!")
         return self.cleaned_data.get('password2')
-
+    
+    def clean_referral_code(self):
+        referral_code = self.cleaned_data.get('referral_code','').strip()
+        if referral_code and not Referral.objects.filter(referral_code=referral_code).exists():
+            forms.ValidationError('Invalid referral code')
+        return referral_code
     
 
 
--- regiapp/urls.py
+++ regiapp/urls.py
@@ -9,7 +9,10 @@ urlpatterns=[
     path('login/',views.login_view,name='login'),
     path('verify_otp/',views.verify_otp,name='verify_otp'),
     path('resend_otp/',views.resend_otp,name='resend_otp'),
-
-]
+    path('forgot_password/',views.forgot_password,name='forgot_password'),
+    path('forgot_otp_verify/',views.forgot_otp_verify,name='forgot_otp_verify'),
+    path('set_new_password/<int:user_id>/',views.set_new_password,name='set_new_password'),
+    path('forgot_resend_otp/',views.forgot_resend_otp,name='forgot_resend_otp'),
+]    
 
 
